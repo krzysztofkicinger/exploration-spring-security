d4af914e9f8d4361bb6c675ce700070f9d0fe205
krzysztofkicinger <krzysztofkicinger@gmail.com>
2017-10-10 15:53:15 +0200

    Database populator (not working)

---
+++ spring-security-boot-app/src/main/java/com/kicinger/spring/security/boot/app/configuration/DataSourceConfiguration.java
@@ -0,0 +1,41 @@
+package com.kicinger.spring.security.boot.app.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("database-authentication")
+public class DataSourceConfiguration {
+
+    @Autowired
+    @Qualifier("dataSource")
+    private DataSource dataSource;
+
+    @Value("classpath:scripts/users.sql")
+    private Resource dataScript;
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer() {
+        final DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+
+    private DatabasePopulator databasePopulator() {
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(dataScript);
+        return populator;
+    }
+
+}
